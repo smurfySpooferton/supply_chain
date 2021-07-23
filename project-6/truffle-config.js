798e6cc1954545ea959f6d5a14fd3b471c17fd51
Daniel Helmbrecht <d.helmbrecht@mobile-shift.com>
2021-07-23 11:56:02 +0200

    finalized contracts, added diagrams, adjusted readme

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,9 +1,23 @@
+const HDWallet= require("@truffle/hdwallet-provider");
+const fs = require('fs');
+const infuraKey = fs.readFileSync("infura").toString().trim();
+const mnemonic = fs.readFileSync("mnemonic").toString().trim();
+
+
 module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*" // Match any network id
+        },
+        rinkeby: {
+            provider: function() {
+                return new HDWallet(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey);
+            },
+            network_id: 4,
+            gas: 4500000,
+            gasPrice: 10000000000,
         }
     },
     compilers: {
